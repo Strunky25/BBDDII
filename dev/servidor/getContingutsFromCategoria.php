831865a678fedff0c83903bbdd5a1f7ec4176471
JSansoP <61268003+JSansoP@users.noreply.github.com>
2021-12-14 14:06:45 +0100

    Fix getContingutsFromCategoriesFav.php
    afegit distinct per evitar repetits

--- dev/servidor/getContingutsFromCategoria.php
+++ dev/servidor/getContingutsFromCategoria.php
@@ -8,7 +8,7 @@ $nomUsuari = $_GET["nomUsuari"];
 
 $conexio = mysqli_connect("localhost", "root", "") or die("Error conectant amb el servidor");
 $bd = mysqli_select_db($conexio, "bd202") or die("Error conectant amb la base de dades");
-$consulta = "SELECT contingut.idContingut, titol, url, nomCategoria FROM usuari JOIN R_Contingut_TipusUsuari ON usuari.tipusUsuari = R_Contingut_TipusUsuari.tipusUsuari JOIN contingut ON contingut.nomCategoria IN (SELECT nomCategoria from R_Categoria_Favorita JOIN Contracte ON contracte.nomUsuari = '$nomUsuari' AND contracte.idContracte = R_Categoria_Favorita.idContracte) AND R_Contingut_TipusUsuari.idContingut = contingut.idContingut";
+$consulta = "SELECT DISTINCT contingut.idContingut, titol, url, nomCategoria FROM usuari JOIN R_Contingut_TipusUsuari ON usuari.tipusUsuari = R_Contingut_TipusUsuari.tipusUsuari JOIN contingut ON contingut.nomCategoria IN (SELECT nomCategoria from R_Categoria_Favorita JOIN Contracte ON contracte.nomUsuari = '$nomUsuari' AND contracte.idContracte = R_Categoria_Favorita.idContracte) AND R_Contingut_TipusUsuari.idContingut = contingut.idContingut";
 $result = mysqli_query($conexio, $consulta);
 if ($result != false && mysqli_num_rows($result) > 0) {
     $resultat = mysqli_fetch_all($result, MYSQLI_ASSOC);
