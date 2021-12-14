3648041d2b6e7b07235bb76e87ea4f258250ddbc
ddawidroch <david.roch.2001@gmail.com>
2021-12-14 19:22:11 +0100

    fix phps wallis

--- BD202/43197070B/deleteVideo.php
+++ BD202/43197070B/deleteVideo.php
@@ -9,15 +9,15 @@ $idContingut = $jsonData->idContingut;
 $url = $jsonData->url;
 $nomCategoria = $jsonData->nomCategoria;
 
-
 $conexio = mysqli_connect("localhost", "root", "") or die("Error conectant amb el servidor");
 $bd = mysqli_select_db($conexio, "bd202") or die("Error conectant amb la base de dades");
 
-$consulta = "DELETE FROM `missatge` WHERE idContingut="$idContingut";
- DELETE FROM `r_contingut_favorit` WHERE idContingut="$idContingut"; 
- DELETE FROM `r_contingut_tipususuari` WHERE idContingut="$idContingut";
- DELETE FROM `contingut` WHERE idContingut="$idContingut;
+$consulta = "DELETE FROM `missatge` WHERE idContingut='$idContingut';
+ DELETE FROM `r_contingut_favorit` WHERE idContingut='$idContingut'; 
+ DELETE FROM `r_contingut_tipususuari` WHERE idContingut='$idContingut';
+ DELETE FROM `contingut` WHERE idContingut='$idContingut'";
 
 $result = mysqli_query($conexio, $consulta);
+mysqli_close($conexio);
 
 echo json_encode($result);
