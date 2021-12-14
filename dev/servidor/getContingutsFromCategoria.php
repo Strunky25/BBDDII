5bfbcf37a6972be4ba07ee7483f63d8124e53c02
JSansoP <61268003+JSansoP@users.noreply.github.com>
2021-12-14 13:31:11 +0100

    Update getContingutsFromCategoria.php

--- dev/servidor/getContingutsFromCategoria.php
+++ dev/servidor/getContingutsFromCategoria.php
@@ -1,15 +1,14 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-if (empty($_GET["idCategoria"]) || empty($_GET["nomUsuari"])) {
+if (empty($_GET["nomUsuari"])) {
     exit("Paràmetres invàlids");
 }
 
 $nomUsuari = $_GET["nomUsuari"];
-$idCategoria = $_GET["idCategoria"];
 
 $conexio = mysqli_connect("localhost", "root", "") or die("Error conectant amb el servidor");
 $bd = mysqli_select_db($conexio, "bd202") or die("Error conectant amb la base de dades");
-$consulta = "SELECT contingut.idContingut, titol, url, nomCategoria FROM usuari JOIN R_Contingut_TipusUsuari ON usuari.tipusUsuari = R_Contingut_TipusUsuari.tipusUsuari JOIN contingut ON contingut.nomCategoria = '$idCategoria' AND R_Contingut_TipusUsuari.idContingut = contingut.idContingut WHERE nomUsuari='$nomUsuari'";
+$consulta = "SELECT contingut.idContingut, titol, url, nomCategoria FROM usuari JOIN R_Contingut_TipusUsuari ON usuari.tipusUsuari = R_Contingut_TipusUsuari.tipusUsuari JOIN contingut ON contingut.nomCategoria IN (SELECT nomCategoria from R_Categoria_Favorita JOIN Contracte ON contracte.nomUsuari = "$nomUsuari" AND contracte.idContracte = R_Categoria_Favorita.idContracte) AND R_Contingut_TipusUsuari.idContingut = contingut.idContingut";
 $result = mysqli_query($conexio, $consulta);
 if ($result != false && mysqli_num_rows($result) > 0) {
     $resultat = mysqli_fetch_all($result, MYSQLI_ASSOC);
