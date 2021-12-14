9c550dfbf27f604df69ac39c9e5424c0e60c4e26
Joan Vilella <j.vilellacandia@gmail.com>
2021-12-14 23:41:27 +0100

    PHP G

--- BD202/BD246276313Z/getRecomanacions.php
+++ BD202/BD246276313Z/getRecomanacions.php
@@ -1,6 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-if (empty($_GET["nomUsuari"])) {
+if (empty($_GET["nomUsuari"]) || empty($_GET["nomCategoria"])) {
     exit("Paràmetres invàlids");
 }
 
@@ -8,4 +8,14 @@ $nomUsuari = $_GET["nomUsuari"];
 
 $conexio = mysqli_connect("localhost", "root", "") or die("Error conectant amb el servidor");
 $bd = mysqli_select_db($conexio, "bd202") or die("Error conectant amb la base de dades");
-$consulta = "SELECT * FROM missatge WHERE Missatge.llegit is false";
+$consulta = "SELECT * FROM Missatge WHERE Missatge.llegit IS FALSE JOIN Contracte ON Missatge.idContracte = Contracte.idContracte AND contracte.nomUsuari = '$nomUsuari' JOIN R_Categoria_Favorita  ON Contracte.idContracte = R_Categoria_Favorita.idContracte AND R_Categoria_Favorita.nomCategoria = '$nomCategoria';
+
+$result = mysqli_query($conexio, $consulta);
+if ($result != false && mysqli_num_rows($result) > 0) {
+    $resultat = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    echo json_encode($resultat);
+} else {
+    echo json_encode(false);
+}
+
+mysqli_close($conexio);
